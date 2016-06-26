428765962a4c13f1f1d19930e7849cf742c9794e
JulianAdams4 <jadams@espol.edu.ec>
2016-06-25 21:05:57 -0500

    Gallery
    Slide/Carousel

--- javascript/pacienteScript.js
+++ javascript/pacienteScript.js
@@ -1,6 +1,16 @@
 $( document ).ready(function() {
+	/*
 	$.getJSON("centros_medicos.json", function(data){
 		console.log(data);
 	});
+	*/
+
+	$('#googleMap').gmap3({
+		marker:{
+      		address: "266 George St, Brisbane QLD 4000"
+    	}
+	});
+
 });
+
 
