1ffa62f82aa7ceeb0564468bc289e07679aaf2e6
JulianAdams4 <jadams@espol.edu.ec>
2016-07-03 15:50:38 -0500

    03072016_1558
    Cargar Servicios desde json

--- javascript/pacienteScript.js
+++ javascript/pacienteScript.js
@@ -1,9 +1,45 @@
-$( document ).ready(function() {
-	/*
-	$.getJSON("centros_medicos.json", function(data){
-		console.log(data);
-	});
-	*/
+function cargar_servicios(){
+    $.getJSON("datos/centros_medicos.json", function(data){
+        $.each(data,function(i) {
+            var $val = data[i].Coordenadas;
+            var $descrip = data[i].Descripcion;
+            var $descrip2 = data[i].Descripcion;
+            var $nombrecentro = data[i].Nombre;
+            
+            var $lat = $val.Latitud
+            var $lon = $val.Longitud
+
+            var $ul = $("<ul></ul>");
+            var $ul2 = $("<ul></ul>");
+            var $mhmt = $(".modal-header");
+            var $h2 = $("<h2></h2>");
+            $h2.attr("class","modal-title");
+            $h2.attr("id","myModalLabel");
+            $h2.text($nombrecentro);
+            $h2.appendTo($mhmt[i]);
+            /*$mhmt[i].innerHTML($nombrecentro);*/
+            var $cp = $(".centroPanel");  // Para paneles
+            var $inf = $(".servicios");  // Para Modal
+            for (var j = 0; j < $descrip.length ; j++){
+                $ul.append("<li>"+$descrip[j]+"</li>");
+                $ul2.append("<li>"+$descrip2[j]+"</li>");
+            }
+            $ul.appendTo($cp[i]);
+            $ul2.appendTo($inf[i]);
+        })
+    });
+}
+
+
+$( document ).ready(function(){
+    cargar_servicios();  // Cargar servicios en Paneles y Modals
+
+
+    /*
+    $.getJSON("centros_medicos.json", function(data){
+        console.log(data);
+    });
+    */
 /*
     $('#googleMap').gmap3({
      map: {
@@ -28,9 +64,19 @@ $( document ).ready(function() {
     initMap();
 
     function initMap() {
+        var $lat = 0.0;
+        var $lon = 0.0;
+        $.getJSON("datos/centros_medicos.json", function(data){
+            $.each(data,function(i) {
+                var $val = data[i].Coordenadas;          
+                var $lat = $val.Latitud
+                var $lon = $val.Longitud
+            });
+        });
+
         var myOptions = {
             zoom: 14,
-            center: new google.maps.LatLng(0.0, 0.0),
+            center: new google.maps.LatLng($lat,$lon),
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         map1 = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
@@ -38,7 +84,7 @@ $( document ).ready(function() {
         map3 = new google.maps.Map(document.getElementById("map_canvas3"), myOptions);
     }
 
-   	var hashTagActive = "";
+    var hashTagActive = "";
     $(".scroll").click(function (event) {
         if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
             event.preventDefault();
@@ -58,5 +104,3 @@ $( document ).ready(function() {
     });
 
 });
-
-
